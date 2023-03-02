a28a99795e98801b72584fbe06856d4fdb1bd87a
21020068_DoanThiMinhHang <48597930+mhjame@users.noreply.github.com>
2023-03-02 09:21:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express') // gọi/ nạp thư viện express
+const app = express() // app is an instance of express. express() - lời gọi hàm
+const port = 3000 // run app ở cổng nào
+
+// app.get (định nghĩa route)
+// get ('/') khi gõ vào chrome / máy sẽ chạy cái server này
+// get('/tin-tuc) khi gõ vào chrome tintuc thì mới nhận
+
+app.get('/trang-chu', (req, res) => {
+    res.send('Hello World!')
+})
+
+/**
+ * (req, res)=>{}  - around function
+ * = function (req, res){
+ *      return res.send('Hello World!')
+ * }
+ */
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
