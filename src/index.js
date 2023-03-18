dbc254ac80d9913f88813a3b85f74e9576a68b7b
21020068_DoanThiMinhHang <48597930+mhjame@users.noreply.github.com>
2023-03-18 15:38:08 +0700

    change path.join()

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 //console.log('Path: ', path.join(__dirname, 'resources\\views'));
 
@@ -48,5 +48,5 @@ app.set('views', path.join(__dirname, 'resources\\views'));
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
