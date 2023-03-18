137d2486a9e7c25076efc626304768b15add1318
21020068_DoanThiMinhHang <48597930+mhjame@users.noreply.github.com>
2023-03-18 16:20:14 +0700

    render khoa hoc - doc doc handlebar.js

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -2,32 +2,24 @@ const Course = require('./models/Course'); // import model
 
 class SiteController {
     //GET / new
-    async index(req, res) {
+    async index(req, res, next) {
+
+        Course.find({})
+            .then(courses => {
+
+                courses = courses.map(course => course.toObject())
+                res.render('home', { courses }) // truyen courses sang home.hdb
+            })
+            .catch(error => next(error));
 
         /*
-        res.json({
-            name: "test"
-        });*/
-
-
-        /* model.find() method in Mongoose no longer accepts a callback function as a parameter
-        Instead, it should be used with a promise or an async/ await syntax
-        This code uses the await keyword to wait for 
-        the Course.find() promise to resolve and assign i
-        ts result to the courses variable. If an error 
-        occurs, it is caught by the try...catch block 
-        and an appropriate error response is sent to 
-        the client.
-        */
 
         try {
             const courses = await Course.find({});
             res.json(courses);
         } catch (err) {
             res.status(400).json({ error: 'ERROR!!!' });
-        }
-
-        //res.render('home');
+        }*/
 
     }
 
