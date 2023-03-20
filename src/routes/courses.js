f3eeb63a61a10ca44399dc29a8997d4e9b2c1ff0
21020068_DoanThiMinhHang <48597930+mhjame@users.noreply.github.com>
2023-03-20 10:44:43 +0700

    delete course, redirect, destroy, bootrap form (delete form)

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -11,8 +11,9 @@ const courseController = require('../app/controllers/CourseController');
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
 router.get('/:id/edit', courseController.edit);
-router.put('/:id', courseController.update)
+router.put('/:id', courseController.update);
 router.get('/:slug', courseController.show);
+router.delete('/:id', courseController.destroy);
 
 module.exports = router;
 //chạy thử husky
