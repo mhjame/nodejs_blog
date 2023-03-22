08ec729d2805e2b2ccdab618ef0aa444cc1a5d99
21020068_DoanThiMinhHang <48597930+mhjame@users.noreply.github.com>
2023-03-22 11:29:41 +0700

    handle checkbox and delete all item was checked

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -10,6 +10,7 @@ const courseController = require('../app/controllers/CourseController');
 //router.get('/:slug', newController.show);
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
+router.post('/handle-form-actions', courseController.handleFormActions);
 router.get('/:id/edit', courseController.edit);
 router.patch('/:id/restore', courseController.restore);
 router.put('/:id', courseController.update);
